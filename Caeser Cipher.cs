faa2407e4119cb55f875ff370d7da81a95599760
grips1 <55134864+grips1@users.noreply.github.com>
2022-02-06 02:31:09 +0200

    Uppercase letters update

--- Caeser Cipher.cs
+++ Caeser Cipher.cs
@@ -1,13 +1,15 @@
 using System;
+using System.Linq;
 namespace suckmynuts
 {
-    class Caeser_Cipher
+    class NewBaseType
     {
         public static void Main()
         {
             int Pindex, length, shift_value;
             int remaining = 0;
             string Message;
+            char[] Alphabet_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
             char[] Alphabet_lowercase = "abcdefghijklmnopqrstuvwxyz".ToCharArray(); // Creats an array of the alphabet (26 chars, 25 spots)
 
             Console.Write("Enter the message you wish to encrypt: ");
@@ -19,24 +21,39 @@ namespace suckmynuts
             Console.WriteLine("Using shift value of " + shift_value + "...");
             char[] MessageArray = Message.ToCharArray(); // Converts message to an array of letters
 
-            length = MessageArray.Length; 
-
-            for(int i = 0; i < length; i++)
+            length = MessageArray.Length;
+            
+            for (int i = 0; i < length; i++)
             {
-                Pindex = Array.IndexOf(Alphabet_lowercase, MessageArray[i]);
+                if (MessageArray[i] == ' ') continue;
+
+                if (Alphabet_lowercase.Contains(Message[i])) // Subroutine for lowercase letters
+                {   
+                    
                 
-                if (Pindex + shift_value > 25)
-                {
-                    remaining = 26 - (Pindex + shift_value); // can give a negative num 
-                    Pindex = -2; 
+                    Pindex = Array.IndexOf(Alphabet_lowercase, MessageArray[i]);
+                    if (Pindex + shift_value > 25)
+                    {
+                        remaining = 26 - (Pindex + shift_value); // can give a negative num 
+                        Pindex = -2;
+                    }
+                    MessageArray[i] = Alphabet_lowercase[Pindex + shift_value - remaining];
+                
+                
                 }
-                if(MessageArray[i] == ' ')
-                    continue;
-                MessageArray[i] = Alphabet_lowercase[Pindex + shift_value - remaining];    
+                else if ((Alphabet_uppercase.Contains(Message[i]))) // Subroutine for uppercase letters
+                {
+                    Pindex = Array.IndexOf(Alphabet_uppercase, MessageArray[i]);
+                    if (Pindex + shift_value > 25)
+                    {
+                        remaining = 26 - (Pindex + shift_value); // can give a negative num 
+                        Pindex = -2;
+                    }
+                    MessageArray[i] = Alphabet_uppercase[Pindex + shift_value - remaining];
+                }            
             }
             string EncryptedMessage = new string(MessageArray);
             Console.WriteLine(EncryptedMessage);
-
         }
     }
 }
