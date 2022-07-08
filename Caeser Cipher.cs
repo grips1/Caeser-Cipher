21f1fb174d9095ed071b50df02d16eadcbc31e9a
fendermcbender <55134864+grips1@users.noreply.github.com>
2022-07-09 00:01:50 +0300

    Cleaned up file

--- Caeser Cipher.cs
+++ Caeser Cipher.cs
@@ -18,9 +18,7 @@ namespace Caeser_Cipher
 			length = MessageArray.Length;
 			for (int i = 0; i < length; i++)
 			{
-                
-				if (MessageArray[i] == ' ') continue;
-				
+				if (MessageArray[i] == ' ') continue;	
 				if (Alphabet_lowercase.Contains(MessageArray[i])) // Subroutine for lowercase letters
 				{
 					Pindex = Array.IndexOf(Alphabet_lowercase, MessageArray[i]);
